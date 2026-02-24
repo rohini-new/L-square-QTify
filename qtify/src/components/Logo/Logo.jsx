16ba897a45f07ed1390e51d9d1148a22f4e12c0c
Rohini Bharti <rohinibharti97@gmail.com>
2026-02-24 19:20:27 +0530

    milestone1 commit 2

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import logo from "../../assets/logo.png"; // adjust path if needed
+import logo from "../../assets/logo.png";
 
 function Logo() {
-  return <img src={logo} alt="Qtify logo" className={styles.logo} />;
+  return <img src={logo} alt="logo" className={styles.logo} />;
 }
 
 export default Logo;
