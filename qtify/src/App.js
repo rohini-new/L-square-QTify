07834232b10a1909e56192b2b5e935387884abc5
Rohini Bharti <rohinibharti97@gmail.com>
2026-02-24 17:51:38 +0530

    milestone1 commit 2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
