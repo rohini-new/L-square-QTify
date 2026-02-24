afd2bb92f7f4163f182be65c1dfac5a80227bbe1
Rohini Bharti <rohinibharti97@gmail.com>
2026-02-24 12:36:49 +0530

    milestone1 commit 2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar searchData={[]} />
       <Hero />
-    </>
+    </BrowserRouter>
   );
 }
 
