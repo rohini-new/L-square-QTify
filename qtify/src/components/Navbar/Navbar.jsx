5ffd8b84a36b1d5de114d2a14267f24be80b5650
Rohini Bharti <rohinibharti97@gmail.com>
2026-02-24 17:57:24 +0530

    milestone1 commit 2

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
@@ -8,9 +7,7 @@ import styles from "./Navbar.module.css";
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-      <Link to="/">
-        <Logo />
-      </Link>
+      <Logo />
       <Search
         placeholder="Search a song of your choice"
         searchData={searchData}
---
+++ qtify/src/config.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://qtify-backend-labs.crio.do";
+
+export default BASE_URL;
